bf91b66e876fd22f710eda09b23d90457d31ba67
mauricio.usatai@gmail.com <mauricio.usatai@gmail.com>
2022-11-21 11:45:17 -0300

    added certs

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,11 @@
 import os
 
 bind = f"0.0.0.0:{os.environ.get('APP_PORT', '8085')}"
+# Certificate
+enviroment = os.environ.get('ENV')
+if enviroment == 'dev':
+  certfile="./certs/dev_certificate.crt"
+  keyfile="./certs/dev_private.key"
+elif enviroment == 'prod':
+  certfile="./certs/prod_certificate.crt"
+  keyfile="./certs/prod_private.key"
